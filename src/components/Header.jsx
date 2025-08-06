30a71b6a03449f0e1b852d3d8b89b3188d4f1a10
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-06 18:17:43 +0300

    making initial dark mode theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,10 @@
+import ThemeToggle from "./ThemeToggle";
+
 function Header() {
   return (
     <header className="flex justify-between w-full mt-16 mb-10">
-      <h1 className="text-4xl  text-white font-bold tracking-[15px] font-josefin">
-        TODO
-      </h1>
-      <img
-        className="w-6 h-6 hover:cursor-pointer"
-        src="/src/assets/moon-icon.svg"
-        alt="moon icon"
-      />
+      <h1 className="text-4xl  font-bold tracking-[15px] font-josefin">TODO</h1>
+      <ThemeToggle />
     </header>
   );
 }
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,5 +1,21 @@
+import React, { useState } from "react";
+
 function ThemeToggle() {
-  return <div></div>;
+  const [isDark, setIsDark] = useState(false);
+
+  const handelThemeClick = () => {
+    document.documentElement.classList.toggle("dark");
+    setIsDark((state) => !state);
+  };
+
+  return (
+    <img
+      className="w-6 h-6 hover:cursor-pointer"
+      src={isDark ? "/src/assets/sun-icon.svg" : "/src/assets/moon-icon.svg"}
+      alt={isDark ? "moon icon" : "sun icon"}
+      onClick={handelThemeClick}
+    />
+  );
 }
 
 export default ThemeToggle;
