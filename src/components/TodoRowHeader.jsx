ec5bb944d5cbaa90fad4d298c1b928ea5722f80a
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-07 23:12:47 +0300

    reading all todos based on show btn click

--- src/components/TodoRowHeader.jsx
+++ src/components/TodoRowHeader.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
-function TodoRowHeader({ addTodo }) {
+import { useQuery } from "@tanstack/react-query";
+import { readTodos } from "../services/todos";
+function TodoRowHeader({ addTodo, onShowTodos }) {
   const [inputValue, setValue] = useState("");
 
   function handleKeyDown(e) {
@@ -11,15 +12,23 @@ function TodoRowHeader({ addTodo }) {
   }
 
   return (
-    <div className="flex items-center h-16 border-b border-[#E3E4F1] rounded-md mb-6 px-4 bg-white">
-      <div className="w-4 h-4 rounded-full border border-[#9495A5] mr-4"></div>
-      <input
-        className="flex-1 bg-transparent outline-none text-sm"
-        placeholder="Create a new todo..."
-        value={inputValue}
-        onChange={(e) => setValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
+    <div className="flex items-center justify-between h-16 border-b border-[#E3E4F1] rounded-md mb-6 px-4 bg-white">
+      <div className="flex ">
+        <div className="w-4 h-4 rounded-full border border-[#9495A5] mr-4"></div>
+        <input
+          className="flex-1 bg-transparent outline-none text-sm"
+          placeholder="Create a new todo..."
+          value={inputValue}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+      <button
+        className="text-sm text-[#9495A5] hover:text-[#494C6B] transition-colors duration-200"
+        onClick={onShowTodos}
+      >
+        Show all todos
+      </button>
     </div>
   );
 }
--- src/components/TodosList.jsx
+++ src/components/TodosList.jsx
@@ -3,8 +3,15 @@ import TodoRowHeader from "./TodoRowHeader";
 import TodoRow from "./TodoRow";
 import { useQuery } from "@tanstack/react-query";
 import { readTodos } from "../services/todos";
+
 function TodosList({ isDark }) {
   const [todos, setTodos] = useState([]);
+  const [showTodos, setShowTodos] = useState(false);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: readTodos,
+    enabled: showTodos,
+  });
   function addTodo(text) {
     if (text.trim() === "") return;
     setTodos((prev) => [...prev, { text }]);
@@ -15,16 +22,13 @@ function TodosList({ isDark }) {
       prevTodos.filter((_, index) => index !== indexToRemove)
     );
   }
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["todos"],
-    queryFn: readTodos,
-  });
-  data.slice(10);
+
   return (
     <div className="w-full">
-      <TodoRowHeader addTodo={addTodo} />
+      <TodoRowHeader addTodo={addTodo} onShowTodos={() => setShowTodos(true)} />
       {isLoading && <p>Loading...</p>}
       {error && <p>{error.message}</p>}
+
       {data?.slice(0, 10).map((todo, index) => (
         <TodoRow key={index} text={todo.title} isDark={isDark} />
       ))}
