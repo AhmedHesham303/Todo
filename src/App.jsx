80335c377ec51fee6c86509d7e1b27cdacf302c6
AhmedHesham <ahmed.hesham303303@gmail.com>
2025-08-07 19:41:22 +0300

    setting react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { use, useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import TodosList from "./components/TodosList";
 import Header from "./components/Header";
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+];
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
 function App() {
+  useQuery({
+    queryKey: ["posts"],
+    queryFn:,
+  });
   const [isDark, setIsDark] = useState(false);
   return (
     <div className="container w-[34rem] flex flex-col items-center justify-center">
